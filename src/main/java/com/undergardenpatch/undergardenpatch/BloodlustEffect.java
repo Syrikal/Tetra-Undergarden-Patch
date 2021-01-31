959391426b59dc99cf49d31ec4c27458db2da7d6
Syric <76715986+Syrikal@users.noreply.github.com>
2021-01-31 15:29:42 -0600

    Added Bloodlust improvement and effect, added frostbite improvements of various strengths

--- src/main/java/com/undergardenpatch/undergardenpatch/BloodlustEffect.java
+++ src/main/java/com/undergardenpatch/undergardenpatch/BloodlustEffect.java
@@ -1,24 +1,20 @@
 package com.undergardenpatch.undergardenpatch;
 
-import com.google.common.collect.Sets;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import se.mickelus.tetra.effect.ItemEffectHandler;
-import se.mickelus.tetra.items.modular.ModularItem;
 import se.mickelus.tetra.effect.ItemEffect;
+import se.mickelus.tetra.items.modular.ModularItem;
 
 
 /**
- * Implementation of a slowing effect.
+ * Implementation of an effect which deals 1.4x damage to Rotspawn.
  */
-public class FrostbiteEffect {
-    private static final ItemEffect frostbite = ItemEffect.get("undergardenpatch:frostbite");
+public class BloodlustEffect {
+    private static final ItemEffect bloodlust = ItemEffect.get("undergardenpatch:bloodlust");
 
     /**
      * Event handler which checks if the mainhand item has our item effect
@@ -27,6 +23,7 @@ public class FrostbiteEffect {
     @SubscribeEvent
     public void attackEvent(LivingHurtEvent event) {
         Entity source = event.getSource().getTrueSource();
+        float damage = event.getAmount();
 
         if (source instanceof PlayerEntity) {
             PlayerEntity player = (PlayerEntity) source;
@@ -34,10 +31,13 @@ public class FrostbiteEffect {
 
             if (heldStack.getItem() instanceof ModularItem) {
                 ModularItem item = (ModularItem) heldStack.getItem();
-                int level = item.getEffectLevel(heldStack, frostbite);
-                
+                int level = item.getEffectLevel(heldStack, bloodlust);
+
                 if (level > 0) {
-                    event.getEntityLiving().addPotionEffect(new EffectInstance(Effects.SLOWNESS, 600, level));
+                    if(event.getEntityLiving().getClassification(false) == EntityClassification.CREATURE) {
+                        event.setAmount(damage * 1.5F);
+                        //event.getEntityLiving().setFire(1); //A very visible way to check if the effect is activating properly
+                    }
                 }
             }
         }
--- src/main/java/com/undergardenpatch/undergardenpatch/UndergardenPatch.java
+++ src/main/java/com/undergardenpatch/undergardenpatch/UndergardenPatch.java
@@ -7,6 +7,7 @@ import net.minecraftforge.fml.common.Mod;
 public class UndergardenPatch {
     public UndergardenPatch() {
         MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new BloodlustEffect());
         MinecraftForge.EVENT_BUS.register(new FrostbiteEffect());
         MinecraftForge.EVENT_BUS.register(new RotbaneEffect());
         MinecraftForge.EVENT_BUS.register(new ThrenodyEffect());
