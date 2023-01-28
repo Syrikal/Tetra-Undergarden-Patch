ba9e195a49399386f39233751fab1060abe6e555
Syric <76715986+Syrikal@users.noreply.github.com>
2023-01-27 21:15:23 -0500

    Added battleaxe replacements

--- src/main/java/com/syric/undergardenpatch/UndergardenPatch.java
+++ src/main/java/com/syric/undergardenpatch/UndergardenPatch.java
@@ -1,20 +1,38 @@
 package com.syric.undergardenpatch;
 
+import com.syric.undergardenpatch.client.RenderBattleaxe;
+import net.minecraftforge.client.event.RenderHandEvent;
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod("undergardenpatch")
 public class UndergardenPatch {
     public static final String MODID = "undergardenpatch";
-
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public UndergardenPatch() {
         MinecraftForge.EVENT_BUS.register(this);
-//        MinecraftForge.EVENT_BUS.register(new BloodlustEffect());
         MinecraftForge.EVENT_BUS.register(new FrostbiteEffect());
         MinecraftForge.EVENT_BUS.register(new FrostnipEffect());
         MinecraftForge.EVENT_BUS.register(new RotbaneEffect());
         MinecraftForge.EVENT_BUS.register(new ThrenodyEffect());
         MinecraftForge.EVENT_BUS.register(new UndermineEffect());
+
+//        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
     }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        MinecraftForge.EVENT_BUS.addListener(this::renderBattleaxeListener);
+        MinecraftForge.EVENT_BUS.addListener(this::renderBattleaxe1pListener);
+    }
+
+    private void renderBattleaxeListener(RenderPlayerEvent event) { RenderBattleaxe.renderBattleaxe(event); }
+    private void renderBattleaxe1pListener(RenderHandEvent event) { RenderBattleaxe.renderBattleaxeFirstPerson(event); }
+
 }
