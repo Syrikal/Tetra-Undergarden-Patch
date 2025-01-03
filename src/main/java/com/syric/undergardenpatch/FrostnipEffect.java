d3a197aab0d6bc21608a4ffba2ec5189cbe682f5
Syric <syric28@gmail.com>
2025-01-03 14:32:34 -0500

    Added bars to indicate effects.

--- src/main/java/com/syric/undergardenpatch/FrostnipEffect.java
+++ src/main/java/com/syric/undergardenpatch/FrostnipEffect.java
@@ -5,10 +5,20 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import se.mickelus.tetra.blocks.workbench.gui.WorkbenchStatsGui;
 import se.mickelus.tetra.effect.ItemEffect;
+import se.mickelus.tetra.gui.stats.bar.GuiStatBar;
+import se.mickelus.tetra.gui.stats.getter.IStatGetter;
+import se.mickelus.tetra.gui.stats.getter.LabelGetterBasic;
+import se.mickelus.tetra.gui.stats.getter.StatGetterEffectLevel;
+import se.mickelus.tetra.gui.stats.getter.TooltipGetterNone;
 import se.mickelus.tetra.items.modular.ModularItem;
+import se.mickelus.tetra.items.modular.impl.holo.gui.craft.HoloStatsGui;
 
 /**
  * Implementation of a slowing effect.
@@ -37,5 +47,14 @@ public class FrostnipEffect {
                 }
             }
         }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void addBars(FMLClientSetupEvent event) {
+        IStatGetter frostnipGetter = new StatGetterEffectLevel(frostnip, 1.0);
+        GuiStatBar frostnipBar = new GuiStatBar(0, 0, 59, "tetra.stats.frostnip", 0.0, 1.0, false, frostnipGetter, LabelGetterBasic.noLabel, new TooltipGetterNone("tetra.stats.frostnip.tooltip"));
+
+        WorkbenchStatsGui.addBar(frostnipBar);
+        HoloStatsGui.addBar(frostnipBar);
     }
 }
--- src/main/java/com/syric/undergardenpatch/ThrenodyLiteEffect.java
+++ src/main/java/com/syric/undergardenpatch/ThrenodyLiteEffect.java
@@ -3,11 +3,20 @@ package com.syric.undergardenpatch;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
+import se.mickelus.tetra.blocks.workbench.gui.WorkbenchStatsGui;
 import se.mickelus.tetra.effect.ItemEffect;
+import se.mickelus.tetra.gui.stats.bar.GuiStatBar;
+import se.mickelus.tetra.gui.stats.getter.LabelGetterBasic;
+import se.mickelus.tetra.gui.stats.getter.StatGetterEffectLevel;
+import se.mickelus.tetra.gui.stats.getter.TooltipGetterNone;
 import se.mickelus.tetra.items.modular.ModularItem;
+import se.mickelus.tetra.items.modular.impl.holo.gui.craft.HoloStatsGui;
 
 import java.util.Objects;
 
@@ -43,5 +52,15 @@ public class ThrenodyLiteEffect {
                 }
             }
         }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void addBars(FMLClientSetupEvent event) {
+        var threnodyGetter = new StatGetterEffectLevel(threnody_lite, 1.0);
+        GuiStatBar threnodyBar = new GuiStatBar(0, 0, 59, "tetra.stats.threnody_lite", 0.0, 1.0, false, threnodyGetter, LabelGetterBasic.noLabel, new TooltipGetterNone("tetra.stats.threnody_lite.tooltip"));
+
+        WorkbenchStatsGui.addBar(threnodyBar);
+        HoloStatsGui.addBar(threnodyBar);
+        UndergardenPatch.LOGGER.debug("Triggered Threnody adding bars");
     }
 }
